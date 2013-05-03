f8516cdebc9eb8d1289196d0beec233d71051d35
Yuan Heng Fan <fanyuanheng@gmail.com>
2013-05-04 00:44:33 +1000

    filtering

--- _includes/js/products.js
+++ _includes/js/products.js
@@ -1,22 +1,16 @@
 <script>
 	$(document).ready(function(){
-    $('.nav li').click(function(){
-    	$('.nav li').removeClass('active');
-    	$(this).addClass('active');
-    });
 
-    function imageFormatter(cellvalue, options, rowObject) {
-			return "<img src='" + cellvalue + "' />";
-  	}
-
-		$.getJSON('assets/products.json', function(response){
+		var reloadProducts = function(products) {
 			$("#products").jqGrid({
 	        datatype: "local",
-	        data: response.products,
+	        data: products,
         	autowidth:true,	
 	        colNames:['', '产品名称', '供应商', '类别', '建议零售价'],
 	        colModel:[
-	        	{name:'image',index:'image', sortable: false, formatter:imageFormatter},
+	        	{name:'image',index:'image', sortable: false, formatter:function(cellvalue, options, rowObject){
+	        		return "<img src='" + cellvalue + "' />";
+	        	}},
 	        	{name:'name',index:'name', sorttype: "text"},
 	          {name:'supplier',index:'supplier', sorttype: "text"},
 	          {name:'category',index:'category', sorttype: "text"},
@@ -33,6 +27,21 @@
           height: '100%'
 	    });
 	    $("#products").jqGrid('navGrid','#pager',{add:false,edit:false,del:false});
+		};
+
+		var filterProducts = function(products, category) {
+			return _.filter(products, function(product){
+				return product.category == category;
+			});
+		};
+
+    $('.nav li').click(function(){
+    	$('.nav li').removeClass('active');
+    	$(this).addClass('active');
+    });
+
+		$.getJSON('assets/products.json', function(response){
+			reloadProducts(filterProducts(response.products, 'vitamins'));
 		});
 
   });
