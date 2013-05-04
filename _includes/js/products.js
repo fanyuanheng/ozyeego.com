ec476473efb95624840ef7c7ae44dcaf3ed41017
Yuan Heng Fan <fanyuanheng@gmail.com>
2013-05-04 16:10:45 +1000

    filter improvement

--- _includes/js/products.js
+++ _includes/js/products.js
@@ -1,6 +1,16 @@
 <script>
 	$(document).ready(function(){
 
+		$.extend($.jgrid.search, {
+        closeOnEscape: true,
+        closeAfterSearch: true,
+        closeAfterReset: true,
+        width: 600,
+        showQuery: true,
+        searchOnEnter: true,
+        sopt: ['cn'],
+    });
+
 		var reloadProducts = function(category) {
 			$.getJSON('assets/products.json', function(response){
 				$("#products").jqGrid({
@@ -9,22 +19,23 @@
 	        	autowidth:true,	
 		        colNames:['', '产品名称', '建议零售价', '易购价', '折扣'],
 		        colModel:[
-		        	{name:'image',index:'image', sortable: false, formatter:function(cellvalue, options, rowObject){
+		        	{name:'image',index:'image', sortable: false, search: false, formatter:function(cellvalue, options, rowObject){
 		        		return "<img src='" + cellvalue + "' />";
 		        	}},
 		        	{name:'name',index:'name', sorttype: "text", formatter:function(cellvalue, options, rowObject){
 		        		return "<span id='product-name' style='word-wrap: break-word;'>" + cellvalue + "</span>";
 		        	}},
-		          {name:'rrp',index:'rrp', sortable: true, formatter:function(cellvalue, options, rowObject){
+		          {name:'rrp',index:'rrp', sortable: true, search: false, formatter:function(cellvalue, options, rowObject){
 		        		return cellvalue ? "<span style='text-decoration: line-through;'>" + cellvalue + "澳元" + "</span>" : '暂缺';
 		        	}},
-		        	{name:'our_price',index:'our_price', sortable: true, formatter:function(cellvalue, options, rowObject){
+		        	{name:'our_price',index:'our_price', sortable: true, search: false, formatter:function(cellvalue, options, rowObject){
 		        		return cellvalue ? "<span style='color: green'>" + cellvalue  + "澳元 + 邮费" + "</span>" : '暂缺';
 		        	}},
-		        	{name:'discount',index:'discount', sortable: true, formatter:function(cellvalue, options, rowObject){
+		        	{name:'discount',index:'discount', sortable: true, search: false, formatter:function(cellvalue, options, rowObject){
 		        		return cellvalue ? "<span style='color: red'>" + cellvalue  + "澳元" + "</span>" : '暂缺';
 		        	}}
 		        ],
+		        ignoreCase: true,
 		        rowNum:5,
 		        rowList:[5,20,50,100],
 		        pager: '#pager',
