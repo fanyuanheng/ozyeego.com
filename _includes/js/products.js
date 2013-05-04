435772076f43953ec31b4b08817e258c1e5bfa7e
Yuan Heng Fan <fanyuanheng@gmail.com>
2013-05-04 11:29:28 +1000

    categorization

--- _includes/js/products.js
+++ _includes/js/products.js
@@ -1,48 +1,54 @@
 <script>
 	$(document).ready(function(){
 
-		var reloadProducts = function(products) {
-			$("#products").jqGrid({
-	        datatype: "local",
-	        data: products,
-        	autowidth:true,	
-	        colNames:['', '产品名称', '供应商', '类别', '建议零售价'],
-	        colModel:[
-	        	{name:'image',index:'image', sortable: false, formatter:function(cellvalue, options, rowObject){
-	        		return "<img src='" + cellvalue + "' />";
-	        	}},
-	        	{name:'name',index:'name', sorttype: "text"},
-	          {name:'supplier',index:'supplier', sorttype: "text"},
-	          {name:'category',index:'category', sorttype: "text"},
-	          {name:'rrp',index:'rrp', sorttype: "text"}
-	        ],
-	        rowNum:20,
-	        rowList:[20,50,100],
-	        pager: '#pager',
-	        gridview:true,
-          rownumbers:true,
-          viewrecords: true,
-          sortorder: 'asc',
-          caption:'产品列表',
-          height: '100%'
+		var reloadProducts = function(category) {
+			$.getJSON('assets/products.json', function(response){
+				$("#products").jqGrid({
+		        datatype: "local",
+		        data: filterProducts(response.products, category),
+	        	autowidth:true,	
+		        colNames:['', '产品名称', '供应商', '类别', '建议零售价'],
+		        colModel:[
+		        	{name:'image',index:'image', sortable: false, formatter:function(cellvalue, options, rowObject){
+		        		return "<img src='" + cellvalue + "' />";
+		        	}},
+		        	{name:'name',index:'name', sorttype: "text"},
+		          {name:'supplier',index:'supplier', sorttype: "text"},
+		          {name:'category',index:'category', sorttype: "text"},
+		          {name:'rrp',index:'rrp', sorttype: "text"}
+		        ],
+		        rowNum:20,
+		        rowList:[20,50,100],
+		        pager: '#pager',
+		        gridview:true,
+	          rownumbers:true,
+	          viewrecords: true,
+	          sortorder: 'asc',
+	          caption:'产品列表',
+	          height: '100%'
+		    }).trigger("reloadGrid");
+		    $("#products").jqGrid('navGrid','#pager',{add:false,edit:false,del:false});
 	    });
-	    $("#products").jqGrid('navGrid','#pager',{add:false,edit:false,del:false});
 		};
 
 		var filterProducts = function(products, category) {
 			return _.filter(products, function(product){
-				return product.category == category;
+				return product.category === category;
 			});
 		};
 
     $('.nav li').click(function(){
     	$('.nav li').removeClass('active');
     	$(this).addClass('active');
+    	var category = $(this).attr('category');
+    	$.getJSON('assets/products.json', function(response){
+	    	$("#products").jqGrid('clearGridData')
+	    		.jqGrid('setGridParam', { data: filterProducts(response.products, category)})
+	  			.trigger('reloadGrid', [{ page: 1}]);
+  		});
     });
 
-		$.getJSON('assets/products.json', function(response){
-			reloadProducts(filterProducts(response.products, 'vitamins'));
-		});
+		reloadProducts('vitamins');
 
   });
 </script>
--- products.md
+++ products.md
@@ -12,11 +12,11 @@ weight: 7
     <div class="container">
       <ul class="nav">
 	      <li class="active" category="vitamins"><a href="#">维他命</a></li>
-		    <li><a href="#">瘦身</a></li>
-		    <li><a href="#">婴儿护理</a></li>
-		    <li><a href="#">美容护肤</a></li>
-		    <li><a href="#">健康食品</a></li>
-		    <li><a href="#">其他</a></li>
+		    <li category="weight loss"><a href="#">瘦身</a></li>
+		    <li category="baby care"><a href="#">婴儿护理</a></li>
+		    <li category="skin care"><a href="#">美容护肤</a></li>
+		    <li category="healthy food"><a href="#">健康食品</a></li>
+		    <li category="others"><a href="#">其他</a></li>
       </ul>
     </div>
   </div>
