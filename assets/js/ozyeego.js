58ac55882c47cd6bbd344a667d9f670ff64e940a
Yuan Heng Fan <yuanfan@nbnco.com.au>
2013-05-10 16:41:51 +1000

    fixed sorting

--- assets/js/ozyeego.js
+++ assets/js/ozyeego.js
@@ -40,13 +40,13 @@ $(document).ready(function(){
             {name:'name',index:'name', sorttype: "text", formatter:function(cellvalue, options, rowObject){
               return "<span id='product-name' style='word-wrap: break-word;'>" + cellvalue + "</span>";
             }},
-            {name:'rrp',index:'rrp', sortable: true, search: false, formatter:function(cellvalue, options, rowObject){
+            {name:'rrp',index:'rrp', sorttype: "float", search: false, formatter:function(cellvalue, options, rowObject){
               return cellvalue ? "<span style='text-decoration: line-through;'>" + cellvalue + "澳元" + "</span>" : '暂缺';
             }},
-            {name:'our_price',index:'our_price', sortable: true, search: false, formatter:function(cellvalue, options, rowObject){
+            {name:'our_price',index:'our_price', sorttype: "float", search: false, formatter:function(cellvalue, options, rowObject){
               return cellvalue ? "<span style='color: green'>" + cellvalue  + "澳元 + 邮费" + "</span>" : '暂缺';
             }},
-            {name:'discount',index:'discount', sortable: true, search: false, formatter:function(cellvalue, options, rowObject){
+            {name:'discount',index:'discount', sorttype: "float", search: false, formatter:function(cellvalue, options, rowObject){
               return cellvalue ? "<span style='color: red'>" + cellvalue  + "澳元" + "</span>" : '暂缺';
             }}
           ],
