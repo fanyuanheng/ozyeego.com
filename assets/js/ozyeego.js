57fb98e73c511148b3f563a2698112818c532174
Yuan Heng Fan <yuanfan@nbnco.com.au>
2013-05-17 17:35:32 +1000

    updated prices

--- assets/js/ozyeego.js
+++ assets/js/ozyeego.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
           datatype: "local",
           data: filterProducts(response.products, category),
           autowidth:true, 
-          colNames:['', '产品名称', '建议零售价', '易购价', '折扣'],
+          colNames:['', '产品名称', '建议零售价', '易购价'],
           colModel:[
             {name:'image',index: 'image', width: 60, sortable: false, search: false, formatter:function(cellvalue, options, rowObject){
               return "<a href='javascript:'><img class='product-thumbnail-link small' data-target='#product-modal' data-toggle='modal' src='" + cellvalue + "' /></a>";
@@ -45,10 +45,7 @@ $(document).ready(function(){
               return cellvalue ? "<span style='text-decoration: line-through;'>" + cellvalue + "澳元" + "</span>" : '暂缺';
             }},
             {name:'our_price',index:'our_price', sorttype: "float", search: false, formatter:function(cellvalue, options, rowObject){
-              return cellvalue ? "<span style='color: green'>" + cellvalue  + "澳元 + 邮费" + "</span>" : '暂缺';
-            }},
-            {name:'discount',index:'discount', sorttype: "float", search: false, formatter:function(cellvalue, options, rowObject){
-              return cellvalue ? "<span style='color: red'>" + cellvalue  + "澳元" + "</span>" : '暂缺';
+              return cellvalue ? "<span style='color: green'>" + cellvalue  + "澳元 + 邮费/手续费" + "</span>" : '暂缺';
             }}
           ],
           hidegrid: false,
